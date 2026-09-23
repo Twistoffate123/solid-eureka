411f57ae8706f85da7a145fb051c37d319c8aea0
Twistoffate123 <deepakthomas28@gmail.com>
2026-09-23 05:58:42 +0000

    Use the candlestick Meridian artwork as the app icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,7 +22,6 @@ export const Route = createRootRoute({
       },
     ],
     links: [
-      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "icon", type: "image/png", sizes: "192x192", href: "/icon-192.png" },
       { rel: "icon", type: "image/png", sizes: "512x512", href: "/icon-512.png" },
       { rel: "stylesheet", href: appCss },
